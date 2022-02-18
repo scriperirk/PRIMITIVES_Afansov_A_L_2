0378cc50a4e5a11d9417f8c7da13db8fe2478c34
Scriper.irk <scriper.irk@gmail.com>
2022-02-18 23:52:34 +0800

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main(String[] args) {
+
+        //переменные
+        int score = 100; //счет
+        int replenishment = 1500; //пополнение
+        int stock = 100; //размер акции за который начисляются бонусы
+        int bonus = 0; //сумма бонусов
+
+        //расчет
+        if (replenishment <= 1000) {
+            score = score + replenishment;
+            System.out.println("Сумма бонусов равна: " + bonus + " , счет составит: " + score);
+        } else {
+            bonus = replenishment / stock;
+            score = score + replenishment + bonus;
+            System.out.println("Сумма бонусов равна: " + bonus + " , счет составит: " + score);
+        }
+    }
 }
